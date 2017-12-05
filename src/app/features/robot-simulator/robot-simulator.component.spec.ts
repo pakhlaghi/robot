00f8607373b601ca95ba4b97f31d31663f784e84
Pakhlaghi <pakhlaghi@agl.com.au>
2017-12-05 23:10:50 +1100

    unit test is added

--- src/app/features/robot-simulator/robot-simulator.component.spec.ts
+++ src/app/features/robot-simulator/robot-simulator.component.spec.ts
@@ -1,28 +1,29 @@
-// import { RobotSimulatorService } from './robot-simulator.service';
-// import { LoggerService } from './../../core/logger.service';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RobotComponent } from './../../shared/robot/robot.component';
+import { RobotSimulatorService } from './robot-simulator.service';
+import { LoggerService } from './../../core/logger.service';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { RobotSimulatorComponent } from './robot-simulator.component';
+import { RobotSimulatorComponent } from './robot-simulator.component';
 
-// describe('RobotSimulatorComponent', () => {
-//   let component: RobotSimulatorComponent;
-//   let fixture: ComponentFixture<RobotSimulatorComponent>;
+describe('RobotSimulatorComponent', () => {
+  let component: RobotSimulatorComponent;
+  let fixture: ComponentFixture<RobotSimulatorComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ RobotSimulatorComponent ],
-//       providers: [RobotSimulatorService, LoggerService]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RobotSimulatorComponent, RobotComponent ],
+      providers: [RobotSimulatorService, LoggerService]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RobotSimulatorComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RobotSimulatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should be created', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
