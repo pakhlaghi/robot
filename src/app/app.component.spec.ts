65d727612e919b3da74789cf106ae084ca13c3e3
Pakhlaghi <pakhlaghi@agl.com.au>
2017-12-06 13:24:54 +1100

    modify unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let fixture: any;
+  let app: any;
+  let el: any;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -13,25 +18,23 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    el = fixture.debugElement.nativeElement;
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
 
   it(`should have as title 'robot simulator'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('robot simulator');
   }));
 
   it('should render title in a div tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.title').textContent).toContain('Welcome to robot simulator!!');
+    expect(el.querySelector('.title').textContent).toContain('Welcome to robot simulator!!');
   }));
 });
