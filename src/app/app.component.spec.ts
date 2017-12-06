c5accf11c471146f6d6cec8fe5c466bad18f5556
Pakhlaghi <pakhlaghi@agl.com.au>
2017-12-06 13:14:50 +1100

    app component unit test is added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,4 +22,16 @@ describe('AppComponent', () => {
   }));
 
 
+  it(`should have as title 'robot simulator'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('robot simulator');
+  }));
+
+  it('should render title in a div tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.title').textContent).toContain('Welcome to robot simulator!!');
+  }));
 });
